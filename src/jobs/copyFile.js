bf8e799eba5f366c53291887f9198fed1adf5e8f
dmfenton <dfenton@esri.com>
2016-04-14 17:32:35 -0400

    integrate s3fs

--- src/jobs/copyFile.js
+++ src/jobs/copyFile.js
@@ -2,8 +2,16 @@ const config = require('config')
 const createKoop = require('koop')
 const koop = createKoop(config)
 
+if (config.filesystem.local) {
+  const LocalFs = require('koop-localfs')
+  koop.register(LocalFs)
+} else {
+  const S3FS = require('koop-s3fs')
+  koop.register(S3FS)
+}
+
 function copyFile (options, done) {
-  koop.files.copy(options, err => done(err))
+  koop.fs.copy(options, err => done(err))
 }
 
 module.exports = copyFile
